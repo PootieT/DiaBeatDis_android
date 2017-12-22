e782df3364a223ed9fe923422550c286e18c3681
Peter Tang <zt8@rice.edu>
2017-12-21 23:18:16 -0600

    add new user registration activity

--- app/src/main/java/com/example/peter/diabeatdis_android/MainActivity.java
+++ app/src/main/java/com/example/peter/diabeatdis_android/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.peter.diabeatdis_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the new user button, view is the button object */
+    public void registerNewUser(View view) {
+        Intent intent = new Intent(this, UserRegistrationActivity.class);
+        startActivity(intent);
+    }
+
 }
