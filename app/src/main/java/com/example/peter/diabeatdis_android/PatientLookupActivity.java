3d1720a3518ab7f49a9b6366ab2ed52a769c16ec
Peter Tang <zt8@rice.edu>
2017-12-26 02:25:22 -0600

    added a lot of different activities

---
+++ app/src/main/java/com/example/peter/diabeatdis_android/PatientLookupActivity.java
@@ -0,0 +1,13 @@
+package com.example.peter.diabeatdis_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientLookupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_lookup);
+    }
+}
